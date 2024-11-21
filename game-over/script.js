7ee82ddeb8b1091f1cf108cac44800c4b4da4de6
Wansiri-D <112197738+Wansiri-D@users.noreply.github.com>
2024-11-21 22:32:09 +0100

    fixed img border

--- game-over/script.js
+++ game-over/script.js
@@ -4,3 +4,13 @@ const guesses = localStorage.getItem('guesses')
 // display
 document.getElementById('word').innerText = word;
 document.getElementById('guesses').innerText = guesses;
+
+// Function for the button "Play Again"
+document.getElementById('play-again').addEventListener('click', () => {
+    window.location.href = 'game.html'
+});
+
+// Function for the button "View Heigh Scores"
+document.getElementById('view-scores').addEventListener('click', () => {
+    window.location.href = 'scores.html'
+});
