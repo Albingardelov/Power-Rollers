bec334c90f81548568afa7dcee8d57e41e44e2e2
Malahat Mortezavi <malahat.m82@gmail.com>
2024-11-30 16:51:50 +0100

    tabort DOM from JS.adding event listener

--- sidan2.js
+++ sidan2.js
@@ -1,31 +1,45 @@
+
+const easyButton = document.querySelector('.easy');
+const mediumButton = document.querySelector('.medium');
+const hardButton = document.querySelector('.hard');
+const buttons = document.querySelectorAll('.btn');
+const message = document.getElementById('message');
+
+/*for changing color and message to user*/
 function handleClick(level) {
-	const buttons = document.querySelectorAll('.btn');
-	const message = document.getElementById('message');
-  
-	// Reset all buttons to orange
-	buttons.forEach(btn => {
-	  btn.style.backgroundColor = 'orange';
-	  btn.classList.remove('shake'); // Remove shake class
-	});
+    buttons.forEach(button => {
+        button.style.backgroundColor = 'rgb(234, 124, 69)';
+        button.classList.remove('shake'); 
+    });
+
 
 	// Change color of clicked button and display message with color
 	if (level === 'easy') {
-		const easyButton = document.querySelector('.easy');
-		easyButton.style.backgroundColor = 'green';
-		easyButton.classList.add('shake'); // Add shake effect
-		message.textContent = 'You chose Easy!';
-		message.style.color = 'green';
-	  } else if (level === 'medium') {
-		const mediumButton = document.querySelector('.medium');
-		mediumButton.style.backgroundColor = 'blue';
-		mediumButton.classList.add('shake'); // Add shake effect
-		message.textContent = 'You chose Medium!';
-		message.style.color = 'blue';
-	  } else if (level === 'hard') {
-		const hardButton = document.querySelector('.hard');
-		hardButton.style.backgroundColor = 'red';
-		hardButton.classList.add('shake'); // Add shake effect
-		message.textContent = 'You chose Hard!';
-		message.style.color = 'red';
-	  }
+        easyButton.style.backgroundColor = 'green';
+        easyButton.classList.add('shake');
+        message.textContent = 'You chose Easy!';
+        message.style.color = 'green';
+    } else if (level === 'medium') {
+        mediumButton.style.backgroundColor = 'blue';
+        mediumButton.classList.add('shake');
+        message.textContent = 'You chose Medium!';
+        message.style.color = 'blue';
+    } else if (level === 'hard') {
+        hardButton.style.backgroundColor = 'red';
+        hardButton.classList.add('shake');
+        message.textContent = 'You chose Hard!';
+        message.style.color = 'red';
+    }
 }
+/* adding event listener for buttons*/
+easyButton.addEventListener('click', function() {
+    handleClick('easy');
+});
+
+mediumButton.addEventListener('click', function() {
+    handleClick('medium');
+});
+
+hardButton.addEventListener('click', function() {
+    handleClick('hard');
+});
