553bbaec8a2e248526a65b53f5fa840cd1c24e92
Malahat Mortezavi <malahat.m82@gmail.com>
2024-11-30 22:56:45 +0100

    Js från malahat branch överförd manuellt till dev branch

--- sidan2.js
+++ sidan2.js
@@ -15,34 +15,46 @@ function handleClick(level) {
 
 	// Change color of clicked button and display message with color
 	if (level === 'easy') {
-		const easyButton = document.querySelector('.easy');
-		easyButton.style.backgroundColor = 'green';
-		easyButton.classList.add('shake'); // Add shake effect
-		message.textContent = 'You chose Easy!';
-		message.style.color = 'green';
-	  } else if (level === 'medium') {
-		const mediumButton = document.querySelector('.medium');
-		mediumButton.style.backgroundColor = 'blue';
-		mediumButton.classList.add('shake'); // Add shake effect
-		message.textContent = 'You chose Medium!';
-		message.style.color = 'blue';
-	  } else if (level === 'hard') {
-		const hardButton = document.querySelector('.hard');
-		hardButton.style.backgroundColor = 'red';
-		hardButton.classList.add('shake'); // Add shake effect
-		message.textContent = 'You chose Hard!';
-		message.style.color = 'red';
-	  }
+        easyButton.style.backgroundColor = 'green';
+        easyButton.classList.add('shake');
+        message.textContent = 'You chose Easy!';
+        message.style.color = 'green';
+    } else if (level === 'medium') {
+        mediumButton.style.backgroundColor = 'blue';
+        mediumButton.classList.add('shake');
+        message.textContent = 'You chose Medium!';
+        message.style.color = 'blue';
+    } else if (level === 'hard') {
+        hardButton.style.backgroundColor = 'red';
+        hardButton.classList.add('shake');
+        message.textContent = 'You chose Hard!';
+        message.style.color = 'red';
+    }
+	
 }
- 
-document.addEventListener('DOMContentLoaded', function () {
-    const audio = new Audio('https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3');
-    audio.play(); // Play the sound
 
-    // Stop the audio after 10 seconds
-    setTimeout(function () {
-        audio.pause(); // Pause the audio
-        audio.currentTime = 0; // Reset the audio to the beginning
-    }, 10000);
+playMusic();
+
+/* adding event listener for buttons*/
+easyButton.addEventListener('click', function() {
+    handleClick('easy');
 });
 
+mediumButton.addEventListener('click', function() {
+    handleClick('medium');
+});
+
+hardButton.addEventListener('click', function() {
+    handleClick('hard');
+});
+/*function for playing music*/
+function playMusic() {
+	const audio = new Audio('https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3'); 
+	audio.play(); 
+  
+	/*stopping music after 10s*/
+	setTimeout(function() {
+	  audio.pause(); 
+	  audio.currentTime = 0; 
+	}, 10000);
+  }
