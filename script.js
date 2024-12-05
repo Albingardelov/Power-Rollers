4072ec6af95468cfaea0c9639f67c8fca3740d28
Malahat Mortezavi <malahat.m82@gmail.com>
2024-12-05 10:42:22 +0100

    change input situation

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 document.getElementById('submitButton').addEventListener('click', function() {
     const nameInput = document.getElementById('nameInput').value;
     const message = document.getElementById('message');
-    
-    if (/^[a-zA-Z\u0600-\u06FF\s]+$/.test(nameInput)) { 
+
+	//for swedish and english names//
+    if (/^[a-zA-ZåäöÅÄÖ\s]+$/.test(nameInput)) { 
         message.textContent = `Hello ${nameInput}! Let's start!`;
         message.style.color = 'green';
 		message.style.fontSize = '25px';
